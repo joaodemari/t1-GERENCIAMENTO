b9e4dc59575146adc3cd4f3d0d4d312c5eb8bd17
Your Name <you@example.com>
2023-09-27 22:12:40 -0300

    feature: classe usuário

---
+++ Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario {
+    private String nome;
+    private String id;
+    private boolean isLogged;
+
+    public Usuario(String nome, String id){
+        this.nome = nome;
+        this.id = id;
+        this.isLogged = false;
+    }
+
+    public void LoginUser(){
+        isLogged = true;
+    }
+
+    public void Logout(){
+        isLogged = false;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    // Criei uma classe usuário de exemplo que pode estar logado ou não
+}
