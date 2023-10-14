3979c9b0fe2c5e7d1ba9636ff4ef793e8a740d7b
João Demari <joaodemari1@gmail.com>
2023-10-14 16:36:13 -0300

    criar menus e logar

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
-public class App{
+import bean.Administrador;
+import bean.Usuario;
+
+public class App {
     public static void main(String[] args) {
         ACMEEmpresa acmeEmpresa = new ACMEEmpresa();
         acmeEmpresa.executa();
---
+++ bean/Administrador.java
@@ -0,0 +1,7 @@
+package bean;
+
+public class Administrador extends Usuario {
+    public Administrador(String nome, String id, String senha) {
+        super(nome, id, senha);
+    }
+}
