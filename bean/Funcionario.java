a8797ac4b26b4b3dee296ecb2d7a4a322b41ae12
Killian D.B <115996000+KillianDB@users.noreply.github.com>
2023-10-20 20:19:19 -0300

    adicionando função getDepartamento

--- bean/Funcionario.java
+++ bean/Funcionario.java
@@ -1,7 +1,14 @@
 package bean;
 
 public class Funcionario extends Usuario {
-    public Funcionario(String nome, String id, String senha) {
+    private String departamento;
+
+    public Funcionario(String nome, String id, String senha, String departamento) {
         super(nome, id, senha);
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento(){
+       return departamento;
     }
 }
