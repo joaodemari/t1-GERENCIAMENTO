fa7174e91c8ba8feb5c24e44d1ee55696cdb05d2
João Demari <joaodemari1@gmail.com>
2023-10-20 19:09:54 -0300

    commit final

--- bean/Funcionario.java
+++ bean/Funcionario.java
@@ -1,7 +1,10 @@
 package bean;
 
 public class Funcionario extends Usuario {
-    public Funcionario(String nome, String id, String senha) {
+    private String departamento;
+
+    public Funcionario(String nome, String id, String senha, String departamento) {
         super(nome, id, senha);
+        this.departamento = departamento;
     }
 }
