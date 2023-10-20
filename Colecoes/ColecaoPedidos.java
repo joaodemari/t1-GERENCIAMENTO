ecfe22aec6e39db997e0eb98195078561c828f6d
Killian D.B <115996000+KillianDB@users.noreply.github.com>
2023-10-20 19:42:52 -0300

    função buscar pedido por departamento

--- Colecoes/ColecaoPedidos.java
+++ Colecoes/ColecaoPedidos.java
@@ -1,5 +1,13 @@
+package Colecoes;
+
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
+import Colecoes.ColecaoUsuarios;
+import bean.Item;
 import bean.Pedido;
+import bean.Usuario;
 
 public class ColecaoPedidos {
 
@@ -15,5 +23,51 @@ public class ColecaoPedidos {
 
     public ArrayList<Pedido> getPedidos() {
         return pedidos;
+    }
+
+    public ArrayList<Pedido> buscaPedidosPorItem(String nome) {
+        ArrayList<Pedido> retorno = new ArrayList<>();
+        for (Pedido pedido : pedidos) {
+            ArrayList<Item> itens = pedido.getItens();
+            for (Item it : itens) {
+                if (it.getNome().contains(nome)) {
+                    retorno.add(pedido);
+                }
+            }
+        }
+        return retorno;
+    }
+
+    public ArrayList<Pedido> buscaPedidoPorFuncionario(String id) {
+        ArrayList<Pedido> retorno = new ArrayList<>();
+        for (Pedido pedido : pedidos) {
+            if (pedido.getUsuario().getId().equals(id)) {
+                retorno.add(pedido);
+            }
+
+        }
+        return retorno;
+    }
+
+    public Pedido buscaPedidoPorNumero(String id) {
+        Pedido p = null;
+        for (Pedido pedido : pedidos) {
+            if (pedido.getId().equals(id)) {
+                p = pedido;
+                break;
+            }
+        }
+        return p;
+    }
+
+public Pedido buscaPedidoPorDepartamento(String id) {
+        Pedido p = null;
+        for (Pedido pedido : pedidos) {
+            if (pedido.getId().equals(id)) {
+                p = pedido;
+                break;
+            }
+        }
+        return p;
     }
 }
